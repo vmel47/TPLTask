b88a846f8a4ab204872857c3ff4384030aeef321
DESKTOP-J1O475E\vmel2 <vmel2947@gmail.com>
2018-05-20 22:22:49 +0300

    Implemented

--- TPLTaskGithub/TPLTaskGithub/Program.cs
+++ TPLTaskGithub/TPLTaskGithub/Program.cs
@@ -1,15 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TPLTaskGithub
 {
     class Program
     {
+        static List<string> data = new List<string>();
+        static string path;
+
         static void Main(string[] args)
         {
+            StartScan();
+        }
+
+        static void StartScan()
+        {
+            Console.WriteLine("Set scan path :");
+            path = Console.ReadLine();
+            Console.WriteLine();
+
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Path not exist =" + path);
+                Console.WriteLine();
+                StartScan();
+            }
+            else
+            {
+                CreateScanTask(path);
+            }
+        }
+
+        static void CreateScanTask(string scanPath)
+        {
+            var task = Task.Factory.StartNew(() =>
+            {
+                data.Add(scanPath);
+                Console.WriteLine(scanPath);
+
+                foreach (string dirPath in Directory.GetDirectories(scanPath))
+                    CreateScanTask(dirPath);
+            });
+            task.Wait();
         }
     }
 }
